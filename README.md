a625a8a3ff31b5a98fd38e5da7fd82dead19bdb6
Agustina Nuñez <aguusnm@gmail.com>
2024-01-24 15:01:53 -0300

    Initial configs

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# docker-mongodb
+A docker to run a MongoDB locally and populate the DB with some initial data.
+# Requirements
+- Docker
+- Bash or something similar to run the commands
+# Usage
+1. Clone the project
+2. Run the following command from the project folder
+   ```
+   docker-compose up
+   ```
+
+To add new data to the DB:
+1. Stop the docker process (Ctrl + C) and remove the volumes created
+   ```
+   docker-compose down --volumes
+   ```
+2. Save changes on *index.js* and run it again:
+   ```
+   docker-compose up
+   ```
+
+The string connection should look like this:
+**mongodb://root:root@127.0.0.1:27017/**
---
+++ index.js
@@ -0,0 +1,88 @@
+/**
+ * HELPERS
+ */
+const toUTC = (stringDate) => (
+  new Date(`${stringDate}T00:00:00Z`)
+);
+
+const generateNumberDigits = (digits) => (
+  Math.floor(Math.random() * 10**digits)
+);
+
+const generateNumberQuantity = (quantity) => (
+  Math.floor(Math.random() * quantity)
+);
+
+const generateDate = () => {
+  const currentDate = new Date();
+  const treeMonthsInMiliseconds = 3 * 30 * 24 * 60 * 60 * 1000;
+  const threeMonthsAgo = new Date(currentDate.getTime() - treeMonthsInMiliseconds);
+
+  const randomTime = Math.random() 
+    * (currentDate.getTime() - threeMonthsAgo.getTime()) 
+    + threeMonthsAgo.getTime();
+  return new Date(randomTime);
+};
+
+const getRandomItem = (list) => (
+  list[generateNumberQuantity(list.length)]
+);
+
+/** 
+ * MOCK SAMPLES 
+ */
+const userOwner = { 
+  _id: UUID('6ddf9759-23fa-4df9-a85d-0c3cb1027fe0'), 
+  username: 'juan.perez'
+};
+const userViewer1 = { 
+  _id: UUID('d3d210e0-956f-4ae2-b3aa-1ded882b1045'), 
+  username: 'paula.lopez'
+};
+const userViewer2 = { 
+  _id: UUID('1af9de4a-825c-4d6e-b15f-f4f575d9c319'), 
+  username: 'ana.ramirez'
+};
+
+const priority = ['high', 'medium', 'low'];
+
+const generateFiles = (length, users) => (
+  Array.from({ length }, () => ({
+    _id: UUID(),
+    nombre: `File ${generateNumberDigits(3)}`,
+    date: generateDate(),
+    priority: getRandomItem(priority),
+    ...users
+  }))
+);
+
+/**
+ * MONGO CONFIG
+ */
+const globalUser = {
+  user: 'root',
+  pwd: 'root',
+  roles: ['readWriteAnyDatabase', 'dbAdminAnyDatabase']
+};
+db = db.getSiblingDB('admin');
+db.createUser(globalUser);
+
+/**
+ * MONGO DATA
+ */
+db = db.getSiblingDB('db1');
+db.createCollection('db1_users');
+db.db1_users.insertMany([/** ... */]);
+
+db = db.getSiblingDB('db2');
+db.createCollection('db2_files');
+
+db.db2_files.insertMany([
+  ...generateFiles(10, {
+    owner: userOwner._id,
+    viewers: [
+      userViewer1._id,
+      userViewer2._id
+    ]
+  })
+]);
